871aa7e723d68be8b23f2019a6b081339d8316cf
Arnaud Thiercelin <athiercelin@icloud.com>
2024-06-01 22:29:45 +0100

    Finished landing on moving platform

---
+++ code/landing-moving-platform/build/CMakeFiles/lander.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/tipoli/Code/lightware-developer/docs/code/landing-moving-platform/sources/dji/application/application.cpp" "CMakeFiles/lander.dir/sources/dji/application/application.cpp.o" "gcc" "CMakeFiles/lander.dir/sources/dji/application/application.cpp.o.d"
+  "/Users/tipoli/Code/lightware-developer/docs/code/landing-moving-platform/sources/lightware/LW20.cpp" "CMakeFiles/lander.dir/sources/lightware/LW20.cpp.o" "gcc" "CMakeFiles/lander.dir/sources/lightware/LW20.cpp.o.d"
+  "/Users/tipoli/Code/lightware-developer/docs/code/landing-moving-platform/sources/lightware/SensorArray.cpp" "CMakeFiles/lander.dir/sources/lightware/SensorArray.cpp.o" "gcc" "CMakeFiles/lander.dir/sources/lightware/SensorArray.cpp.o.d"
+  "/Users/tipoli/Code/lightware-developer/docs/code/landing-moving-platform/sources/main.cpp" "CMakeFiles/lander.dir/sources/main.cpp.o" "gcc" "CMakeFiles/lander.dir/sources/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/landing-moving-platform/build/CMakeFiles/lander.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for lander.
+# This may be replaced when dependencies are built.
